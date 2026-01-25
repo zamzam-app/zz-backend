b0079b90a8a16034062f64131f6217e481bd59ea
Abin Anto <abin98anto@gmail.com>
2026-01-25 23:46:06 +0530

    feat: added roles guard in products

--- src/controller/product/product.controller.ts
+++ src/controller/product/product.controller.ts
@@ -6,11 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import {
   ApiProductCreate,
   ApiProductFindAll,
@@ -18,37 +19,48 @@ import {
   ApiProductRemove,
   ApiProductUpdate,
 } from './dto/product.swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { UserRole } from '../users/entities/user.entity';
 
 @ApiTags('product')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @Roles(UserRole.ADMIN)
   @ApiProductCreate()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
   @Get()
+  @Roles(UserRole.USER, UserRole.MANAGER, UserRole.ADMIN)
   @ApiProductFindAll()
   findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
+  @Roles(UserRole.USER, UserRole.MANAGER, UserRole.ADMIN)
   @ApiProductFindOne()
   findOne(@Param('id') id: string) {
     return this.productService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(UserRole.ADMIN)
   @ApiProductUpdate()
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Roles(UserRole.ADMIN)
   @ApiProductRemove()
   remove(@Param('id') id: string) {
     return this.productService.remove(id);
