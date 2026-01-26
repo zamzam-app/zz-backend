6c11a244b87b9ce470fa62fa81f31f706617449d
Abin Anto <abin98anto@gmail.com>
2026-01-26 17:29:00 +0530

    refactor: moved swagger to form.swagger.ts

--- src/controller/forms/dto/create-form.dto.ts
+++ src/controller/forms/dto/create-form.dto.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsEnum,
@@ -13,102 +12,46 @@ import { Type } from 'class-transformer';
 import { QuestionType, Option } from '../entities/form.entity';
 
 export class QuestionDto {
-  @ApiProperty({
-    example: 'short_answer',
-    description: 'Type of the question',
-    enum: QuestionType,
-  })
   @IsEnum(QuestionType)
   @IsNotEmpty()
   type: QuestionType;
 
-  @ApiProperty({
-    example: 'What is your name?',
-    description: 'Title of the question',
-  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
-  @ApiProperty({
-    example: 'Please enter your full name',
-    description: 'Hint for the question',
-    required: false,
-  })
   @IsString()
   @IsOptional()
   hint?: string;
 
-  @ApiProperty({
-    example: [{ text: 'Option 1' }, { text: 'Option 2' }],
-    description: 'Array of options for multiple choice or checkbox questions',
-    type: [Object],
-    required: false,
-  })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => Object)
   @IsOptional()
   options?: Option[];
 
-  @ApiProperty({
-    example: true,
-    description: 'Whether the question is required',
-  })
   @IsNotEmpty()
   isRequired: boolean;
 
-  @ApiProperty({
-    example: 5,
-    description: 'Maximum rating for star rating questions',
-    required: false,
-  })
   @IsNumber()
   @IsOptional()
   maxRatings?: number;
 }
 
 export class CreateFormDto {
-  @ApiProperty({
-    example: 'Customer Feedback Form',
-    description: 'Title of the form',
-  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
-  @ApiProperty({ example: 1, description: 'Version number of the form' })
   @IsNumber()
   @IsOptional()
   version?: number;
 
-  @ApiProperty({
-    example: [
-      {
-        type: QuestionType.ShortAnswer,
-        title: 'What is your name?',
-        hint: 'Please enter your full name',
-        isRequired: true,
-      },
-      {
-        type: QuestionType.Paragraph,
-        title: 'Tell us about yourself',
-        isRequired: false,
-      },
-    ],
-    description: 'Array of form questions',
-    type: [QuestionDto],
-  })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => QuestionDto)
   questions: QuestionDto[];
 
-  @ApiProperty({
-    example: '60d5ecb86217152c9043e02d',
-    description: 'Associated user ID',
-    required: false,
-  })
   @IsMongoId()
   @IsOptional()
   userId?: string;
--- src/controller/forms/dto/form.swagger.ts
+++ src/controller/forms/dto/form.swagger.ts
@@ -4,8 +4,89 @@ import {
   ApiOkResponse,
   ApiCreatedResponse,
   ApiNotFoundResponse,
+  ApiProperty,
 } from '@nestjs/swagger';
-import { Form } from '../entities/form.entity';
+import { Form, QuestionType } from '../entities/form.entity';
+
+export class QuestionDtoSwagger {
+  @ApiProperty({
+    example: 'short_answer',
+    description: 'Type of the question',
+    enum: QuestionType,
+  })
+  type: QuestionType;
+
+  @ApiProperty({
+    example: 'What is your name?',
+    description: 'Title of the question',
+  })
+  title: string;
+
+  @ApiProperty({
+    example: 'Please enter your full name',
+    description: 'Hint for the question',
+    required: false,
+  })
+  hint?: string;
+
+  @ApiProperty({
+    example: [{ text: 'Option 1' }, { text: 'Option 2' }],
+    description: 'Array of options for multiple choice or checkbox questions',
+    type: [Object],
+    required: false,
+  })
+  options?: object[];
+
+  @ApiProperty({
+    example: true,
+    description: 'Whether the question is required',
+  })
+  isRequired: boolean;
+
+  @ApiProperty({
+    example: 5,
+    description: 'Maximum rating for star rating questions',
+    required: false,
+  })
+  maxRatings?: number;
+}
+
+export class CreateFormDtoSwagger {
+  @ApiProperty({
+    example: 'Customer Feedback Form',
+    description: 'Title of the form',
+  })
+  title: string;
+
+  @ApiProperty({ example: 1, description: 'Version number of the form' })
+  version?: number;
+
+  @ApiProperty({
+    example: [
+      {
+        type: QuestionType.ShortAnswer,
+        title: 'What is your name?',
+        hint: 'Please enter your full name',
+        isRequired: true,
+      },
+      {
+        type: QuestionType.Paragraph,
+        title: 'Tell us about yourself',
+        isRequired: false,
+      },
+    ],
+    description: 'Array of form questions',
+    type: [QuestionDtoSwagger],
+  })
+  questions: QuestionDtoSwagger[];
+
+  @ApiProperty({
+    example: '60d5ecb86217152c9043e02d',
+    description: 'Associated user ID',
+    required: false,
+  })
+  userId?: string;
+}
 
 export function ApiFormCreate() {
   return applyDecorators(
