d33bff671450eeaf52689e4475cc1bcae60035fc
Abin Anto <abin98anto@gmail.com>
2026-02-23 15:40:19 +0530

    release: v0.0.4

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:3001',
       'http://localhost:5173',
       'http://localhost:5174',
       'http://127.0.0.1:5174',
