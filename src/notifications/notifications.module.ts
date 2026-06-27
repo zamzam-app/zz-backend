71887c82e9ab7559f7ecdf843bf030bc8be7f351
Abin Anto <abin98anto@gmail.com>
2026-06-27 13:07:31 +0530

    chore: removed dead token from notification service

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from '../controller/users/users.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, UsersModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
